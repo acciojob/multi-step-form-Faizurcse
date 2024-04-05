376b870119144792c2245a860750affeb92cd6f6
Faizurcse <mdfaizurrahman2000@gmail.com>
2024-04-06 04:06:36 +0530

    multi

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,47 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import CoustomerDetails from "./CoustomerDetails";
+import Cars from "./Cars";
+import Payment from "./Payment";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [step, setStep] = useState(1);
 
-export default App
+  const nextStep = (e) => {
+    e.preventDefault()
+    setStep(step + 1);
+  };
+
+  const prevStep = () => {
+    setStep(step - 1);
+  };
+
+ function Submitform(e){
+  e.preventDefault()
+  console.log(e.target.value)
+ }
+
+  return (
+    <div style={{display:'flex',justifyContent:'center'}}>
+      <div
+        style={{
+          backgroundColor: "skyblue",
+          width: "25%",
+          height: "70%",
+          borderRadius: "5px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        {step === 1 && <CoustomerDetails nextStep={nextStep} />}
+        {step === 2 && <Cars nextStep={nextStep} prevStep={prevStep} />}
+        {step === 3 && <Payment prevStep={prevStep} Submitform={Submitform} />}
+
+        
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Cars.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Cars({nextStep,prevStep} ){
+    
+    return(
+        <form onSubmit={nextStep} >
+            <div  style={{height:'250px'}}>
+                <h3>Car Deatils</h3>
+                <label htmlFor="model">Car model :</label><br /><br />
+                <input type="text"  id="model" required/><br /><br />
+                <label htmlFor="car_price">Price :</label><br /><br />
+                <input type="number"  id="car_price" required/><br /><br />
+                <button onClick={prevStep} >Previous</button>
+                <button style={{marginLeft:'50px'}} type='submit'>Next</button>
+            </div>
+        </form>
+    )
+}
+
+export default Cars
